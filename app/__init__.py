b1a77ab3a2d4b8a1a4905fc2821e01cb8d69b280
saidino84 <tylorguy2018@gmail.com>
2021-07-20 00:13:16 -0400

    Adicionando procfile

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,7 @@ from flask import Flask, render_template,request,url_for,redirect,jsonify,sessio
 
 def create_app():
     app=Flask(__name__)
-
     app.add_url_rule('/', view_func=index)
-
     return app
 
 def index():
