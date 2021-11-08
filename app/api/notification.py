c6c0bb02b6e0ea400e481d1278b768e1ceb993c4
saidino84 <tylorguy2018@gmail.com>
2021-11-08 19:43:26 +0200

    RECEBENDO MESSANGENS DE NOTIFICATION PARA SPAR APP adding requests module

--- app/api/notification.py
+++ app/api/notification.py
@@ -18,8 +18,12 @@ def postproduct():
         'notification':{
         'message':str(request.json['notification']['message']),
         'title':str(request.json['notification']['title']),
-
-        'image_url':str(request.json['notification']['image_url'])}}
+        'image':str(request.json['notification']['image_url'])},
+        "data":{
+            "body":str(request.json['notification']['message']),
+            "title":str(request.json['notification']['title']),
+            "image":str(request.json['notification']['image_url'])
+        }}
         # print(f'Body:{message},\nTile {message}, \nImage: {image_url}')
         auth='key=AAAAT7I801w:APA91bFyk0q5leLPRR8YtKy3Q1s1wA38IcLdhZbkXF3ysCY7HdwOZ2IJEMQoaO8t5zKTwE_A8hockdmrL1FgkKkbqfK6FX4eCP5R7o1XLliD-e3OYFY1aruXPxac9rc_x6Dr6ly17nER'
 
