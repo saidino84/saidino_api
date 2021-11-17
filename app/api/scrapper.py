e731876bac4a9efb7f3dcdc07e9bc629b203c154
saidino84 <tylorguy2018@gmail.com>
2021-11-17 06:25:23 +0200

    Adicionando scrapper aprimorado

--- app/api/scrapper.py
+++ app/api/scrapper.py
@@ -11,17 +11,36 @@ def index():
     # return jsonify([{'name':'saidie','idade':12}])
     return render_template('post.html',titles=titles,products=[])
 
-@scraper.route("/scrap/", methods=['POST'])
+@scraper.route('/admin',methods=['POST','GET'])
+def admin():
+    value =request.json
+    print(value)
+    return jsonify([{"you":'saidino','lover':'yes',"data":f'{value}'}])
+@scraper.route("/scrap", methods=['POST','GET'])
 def scrap():
-    url=request.form['uri']
-    data=request.data
-    print('form',url);
-    print('data ',data);
-    uri='';
-    if url !=None:
+
+    uri='=';
+    description=' ( A DETERMINAR ...)'
+    print('GETTING DATAS...')
+    try:
+        url=request.form['uri']
+        print(f" posted by form :url {url}")
         uri=url
-    else:
-        uri=data['uri']
+        
+    except  Exception as e:
+        try:
+            json=request.json
+            print(f'from json {json}')
+            if json !=None:
+                uri=json['uri']
+                description=json['description']
+        except  Exception as e:
+            printf("sem  link reconecido {e}")
+    
+    print(f'by json post {uri}')
+
+    if len(uri)<8:
+        return "link invalid"
     print(f'{uri} has got')
     data =rq.get(uri).text
     list_product_xpath ='/html/body/div[1]/div[3]/div/div/div/div[4]/ul'
@@ -39,13 +58,13 @@ def scrap():
         image_og = product.select('a img')[0]['data-original']
         image = product.select('a img')[0]['src']
         price = product.find('span', class_='price').select('bdi')[0].get_text()
-        print(f"\033[32m {title} \033[0m \033[34m {price} \033[0m")
+        print(f"\033[32m {title} \033[0m \033[34m {price} \033[0m DESC: \033[36m{description}\033[m")
         # details=get_details(detais_uri)
         products.append(
                 {'product_name':title,
                     'url_img':image_og,
                     'prod_details':detail_uri,
-                    # 'description':FRUITS_DESCRIPTION,
+                    'description':description,
                     'price':price,#details['price'],
                     'category':None,#details['category'],
                     'medida':None,#details['medida'],
